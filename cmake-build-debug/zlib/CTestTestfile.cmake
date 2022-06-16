6ca4d1769df1f18086dbe577d09a80918fbc5f9d
The-Elfinator <49344944+The-Elfinator@users.noreply.github.com>
2022-06-16 17:37:39 +0300

    Add files via upload

---
+++ cmake-build-debug/zlib/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/artem/CLionProjects/Lab2PngToPnm/zlib
+# Build directory: C:/Users/artem/CLionProjects/Lab2PngToPnm/cmake-build-debug/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/artem/CLionProjects/Lab2PngToPnm/zlib/CMakeLists.txt;235;add_test;C:/Users/artem/CLionProjects/Lab2PngToPnm/zlib/CMakeLists.txt;0;")
+add_test(example64 "example64")
+set_tests_properties(example64 PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/artem/CLionProjects/Lab2PngToPnm/zlib/CMakeLists.txt;244;add_test;C:/Users/artem/CLionProjects/Lab2PngToPnm/zlib/CMakeLists.txt;0;")
---
+++ cmake-build-debug/zlib/cmake_install.cmake
@@ -0,0 +1,120 @@
+# Install script for directory: C:/Users/artem/CLionProjects/Lab2PngToPnm/zlib
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Lab2PngToPnm")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "C:/CLion-2021.3.2/bin/mingw/bin/objdump.exe")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files (x86)/Lab2PngToPnm/lib/libzlib.dll.a")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files (x86)/Lab2PngToPnm/lib" TYPE STATIC_LIBRARY OPTIONAL FILES "C:/Users/artem/CLionProjects/Lab2PngToPnm/cmake-build-debug/zlib/libzlib.dll.a")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files (x86)/Lab2PngToPnm/bin/libzlib.dll")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files (x86)/Lab2PngToPnm/bin" TYPE SHARED_LIBRARY FILES "C:/Users/artem/CLionProjects/Lab2PngToPnm/libzlib.dll")
+  if(EXISTS "$ENV{DESTDIR}/C:/Program Files (x86)/Lab2PngToPnm/bin/libzlib.dll" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/C:/Program Files (x86)/Lab2PngToPnm/bin/libzlib.dll")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "C:/CLion-2021.3.2/bin/mingw/bin/strip.exe" "$ENV{DESTDIR}/C:/Program Files (x86)/Lab2PngToPnm/bin/libzlib.dll")
+    endif()
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files (x86)/Lab2PngToPnm/lib/libzlibstatic.a")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files (x86)/Lab2PngToPnm/lib" TYPE STATIC_LIBRARY FILES "C:/Users/artem/CLionProjects/Lab2PngToPnm/cmake-build-debug/zlib/libzlibstatic.a")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files (x86)/Lab2PngToPnm/include/zconf.h;C:/Program Files (x86)/Lab2PngToPnm/include/zlib.h")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files (x86)/Lab2PngToPnm/include" TYPE FILE FILES
+    "C:/Users/artem/CLionProjects/Lab2PngToPnm/cmake-build-debug/zlib/zconf.h"
+    "C:/Users/artem/CLionProjects/Lab2PngToPnm/zlib/zlib.h"
+    )
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files (x86)/Lab2PngToPnm/share/man/man3/zlib.3")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files (x86)/Lab2PngToPnm/share/man/man3" TYPE FILE FILES "C:/Users/artem/CLionProjects/Lab2PngToPnm/zlib/zlib.3")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files (x86)/Lab2PngToPnm/share/pkgconfig/zlib.pc")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files (x86)/Lab2PngToPnm/share/pkgconfig" TYPE FILE FILES "C:/Users/artem/CLionProjects/Lab2PngToPnm/cmake-build-debug/zlib/zlib.pc")
+endif()
+
